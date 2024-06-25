188ae31d17be43ada84308d4bbf18f4362798403
tmbc12 <reachus@tmbc.in>
2024-06-25 12:58:56 +0530

    code update

---
+++ src/app/pages/home/homepage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from "./homepage.module.css";
+import Image from 'next/image';
+import Hero from "../../../../public/assets/img/hero.png";
+import Sea from "../../../../public/assets/img/Sea.png"
+
+function homepage() {
+  return (
+    <div>
+        {/* first section */}
+        <div className={styles.firstcontainer}>
+            <Image src={Hero} className={styles.Heroimg}/>
+        </div>
+
+        {/* second section */}
+        <div className={styles.secondcontainer}>
+            <div>
+                <Image src={Sea} className={styles.seaimg}/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default homepage
